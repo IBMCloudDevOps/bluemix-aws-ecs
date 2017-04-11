594d81c4421463d793341c6f0903a0af542b681f
Jesse Antoszyk <jcantosz@us.ibm.com>
2017-04-10 22:00:49 -0400

    Initial commit
    Signed-off-by: Jesse Antoszyk <jcantosz@us.ibm.com>

---
+++ README.md
@@ -0,0 +1,3 @@
+## Create an IBM DevOps Services toolchain
+[![Create Toolchain](https://console.ng.bluemix.net/devops/graphics/create_toolchain_button.png)](https://console.ng.bluemix.net/devops/setup/deploy/?repository=https://github.com/IBMCloudDevOps/bluemix-aws-ecs)
+
---
+++ install_ansible.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+python --version
+echo "apt-get -qq update"
+sudo apt-get -qq update
+echo "apt-get -qq -y install python-dev libssl-dev libffi-dev"
+sudo apt-get -qq -y install python-dev libssl-dev libffi-dev
+
+# Boto needed for s3 & elastic-beanstalk
+echo "pip install pycrypto pyyaml boto boto3 botocore ansible --quiet"
+sudo pip install pycrypto pyyaml boto boto3 botocore ansible --quiet
+
+echo "apt-get -qq clean"
+sudo apt-get -qq clean
+
